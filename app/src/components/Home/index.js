5ad806798c5b9119db60ba6bc90b560884904baa
Alejandro Rivas <jarlalejor@gmail.com>
2018-07-14 21:14:00 -0500

    deployment

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -4,7 +4,7 @@ import './style.css'
 const Home = props => (
   <section className='HomeJarl' id='home'>
     <h1 className='HomeJarl-title'>Alejandro Rivas</h1>
-    <h3 className='HomeJarl-text'>Arquitecto fron-end & fullStack developer</h3>
+    <h3 className='HomeJarl-text'>Arquitecto front-end & fullStack developer</h3>
   </section>
 )
 
