5d25a90dc15418ff5c005915824e51d5f452e2af
Alejandro Rivas <jarlalejor@gmail.com>
2019-03-19 22:09:55 -0500

    s

--- jarl/src/Pages/Experiences/Project.js
+++ jarl/src/Pages/Experiences/Project.js
@@ -13,6 +13,12 @@ class Project extends PureComponent {
           <p>
           Una de las experiencias más emocionantes fue la integración de todo el sistema frontal con la aplicación Ruby on Rails y la animación de cambio de página.
           </p>
+          <p>
+          Como Desarrollador de Front-end , fui responsable de crear la IU completa para el nuevo sitio web, redefinir la Experiencia del usuario y estudiar las nuevas interacciones entre el Usuario y la Interfaz.
+          </p>
+          <p>
+          Una de las experiencias más emocionantes fue la integración de todo el sistema frontal con la aplicación Ruby on Rails y la animación de cambio de página.
+          </p>
         </div>
 
         <ul className="Project-tags">
