6ce1d432c529681435eca275560bfb6e9b567f31
Alejandro Rivas <jarlalejor@gmail.com>
2019-04-18 22:44:53 -0500

    db de tecnologias

---
+++ jarl/src/Pages/Technology/db.js
@@ -0,0 +1,16 @@
+const TECHNOLOGY = [
+  { name: "JavaScript", logo: "", descripttion: "Javascript es un lenguaje con muchas posibilidades, utilizado para crear pequeños y grandes app" },
+  { name: "React.js", logo: "", descripttion: "React es una biblioteca Javascript de código abierto diseñada para crear interfaces de usuario con el objetivo de facilitar el desarrollo de aplicaciones en una sola página. Es mantenido por Facebook y la comunidad de software libre" },
+  { name: "Node.js", logo: "", descripttion: "Es un entorno en tiempo de ejecución multiplataforma, de código abierto, para la capa del servidor" },
+  { name: "Django", logo: "", descripttion: "Django es un framework de desarrollo web de código abierto, escrito en Python, que respeta el patrón de diseño conocido como Modelo–vista–template." },
+  { name: "Python", logo: "", descripttion: "Python es un lenguaje de programación interpretado cuya filosofía hace hincapié en una sintaxis que favorezca un código legible" },
+  { name: "MongoDB", logo: "", descripttion: "Es un sistema de base de datos NoSQL orientado a documentos de código abierto. En lugar de guardar los datos en tablas, tal y como se hace en las bases de datos relacionales, MongoDB guarda estructuras de datos BSON " },
+  { name: "MYSQL", logo: "", descripttion: "Es un sistema de gestión de bases de datos relacional desarrollado " },
+  { name: "HTML5", logo: "", descripttion: "Es un lenguaje de marcado para la elaboración de páginas web." },
+  { name: "CSS3", logo: "", descripttion: 'CSS, en español "Hojas de estilo en cascada", es un lenguaje de diseño gráfico para definir y crear la presentación de un documento estructurado escrito en un lenguaje de marcado' },
+  { name: "GNU/Linux", logo: "", descripttion: "Linux es un sistema operativo de software libre Es un sistema multitarea, multiusuario, compatible con UNIX, y proporciona una interfaz de comandos y una interfaz gráfica, que lo convierte en un sistema muy atractivo y con estupendas perspectivas de futuro." },
+  { name: "DevOps", logo: "", descripttion: "DevOps es la mezcla de tareas realizadas por los equipos de desarrollo de aplicaciones y sistemas de una empresa. " },
+  { name: "Responsive web Design", logo: "", descripttion: "El diseño web adaptable, es una filosofía de diseño y desarrollo cuyo objetivo es adaptar la apariencia de las páginas web al dispositivo que se esté utilizando para visitarlas" },
+]
+
+export default TECHNOLOGY
